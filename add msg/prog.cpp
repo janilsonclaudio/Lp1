bb8c9ea0f0849ac416a92611206402eab9e0de6f
janilsonclaudio <47826023+janilsonclaudio@users.noreply.github.com>
2020-06-18 17:27:29 -0300

    Update prog.cpp

--- add msg/prog.cpp
+++ add msg/prog.cpp
@@ -2,34 +2,60 @@
 #include <string>
 #include <string.h>
 #include <fstream>
+using namespace std;
 
 int main(int argc, char *argv[]){
-	std::string mensagem;
+	string mensage;
 	if(argc == 1){ //caso o parametro add não tenha sido digitado
-		std::cout << argv[0] << "Uso: add <'mensagem'>" << std::endl; //mensagem enviada pelo programa para informar o usuario como o programa deve ser utilizado
+		cout << argv[0] << "Uso: add <'mensage'> or list" << endl; //mensage enviada pelo programa para informar o usuario como o programa deve ser utilizado
 		return 1;
  	}
 	else if(argc == 2 && strcmp(argv[1],"add") == 0){
-		std::cout << "Digite sua mensagem: " << std::endl;
-		std::getline(std::cin, mensagem);
-		std::ofstream diary;
-		diary.open("diary.txt",std::ios::app);
-		diary << mensagem;
-		diary.close();
-		std::cout << "Mensagem Adicionada" << std::endl;
+		cout << "Digite sua mensage: " << endl;
+		getline(cin, mensage);
+		ofstream writer;
+		writer.open("diary.txt",ios::app);
+		if(!writer.is_open()){
+			cerr << "arquivo não pode ser criado ou acessado";
+			return 1;
+		}
+		writer << mensage << endl;
+		writer.close();
+		cout << "Mensagem Adicionada" << endl;
 		return 0;
 	}
 	else if(argc == 3 && strcmp(argv[1],"add") == 0){
-		mensagem = argv[2];
-		std::ofstream diary;
-		diary.open("diary.txt",std::ios::app);
-		diary << mensagem;
-		diary.close();
-		std::cout << "Mensagem Adicionada" << std::endl;
+		mensage = argv[2];
+		ofstream writer;
+		writer.open("diary.txt",ios::app);
+		if(!writer.is_open()){
+			cerr << "arquivo não pode ser criado ou acessado";
+			return 1;
+		}
+		writer << mensage << endl;
+		writer.close();
+		cout << "Mensagem Adicionada" << endl;
 		return 0;
 	}
+	else if(argc == 2 && strcmp(argv[1],"list") == 0){
+		string text;
+		ifstream reader;
+		reader.open("diary.txt");
+		if(!reader.is_open()){
+			cerr << "arquivo não pode ser acessado";
+			return 1;
+		}
+		while(!reader.eof()){
+			getline(reader, text);
+			if(text.size() == 0){
+				break;
+			}
+			cout << text << endl;
+		}
+		reader.close();
+	}
 	else{
-		std::cout << argv[0] << "Uso: <add 'mensagem'>" << std::endl; //mensagem enviada pelo programa para informar o usuario como o programa deve ser utilizado
+		cout << argv[0] << "Uso: add <'mensage'> or list" << endl; //mensage enviada pelo programa para informar o usuario como o programa deve ser utilizado
 		return 1;
 	}
 }
